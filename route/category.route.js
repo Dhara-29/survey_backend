cf403373100ccc665a8b98bf2db42de5e094720e
Dhara Sonbhurra <dharasonbhurra29@gmail.com>
2024-08-25 21:27:33 +0530

    removed

--- route/category.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addCategory, getAllCategories, removeCategory } from "../controller/category.controller.js";
-import { authenticateToken } from "./user.route.js";
-const app = express.Router();
-app.post('/addCategory',addCategory);
-app.delete('/removeCategory',removeCategory);
-app.get('/getAllCategories',getAllCategories);
-export default app;
